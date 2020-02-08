ee4b2274b1c164e869e8cd73d5d6bb32f3ee66d4
Alec M. Harb <harba@CF167-21.cs.wwu.edu>
2020-02-07 16:03:31 -0800

    parse file

---
+++ src/Parse.java
@@ -0,0 +1,13 @@
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import java.io.File;
+
+public class Parse {
+}
