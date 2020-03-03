66362c7cadbe8e32c3692a5c2450fa81c7cec5d0
harba000 <38176868+harba000@users.noreply.github.com>
2020-03-02 19:40:49 -0800

    Create Shot.java

---
+++ src/Shot.java
@@ -0,0 +1,17 @@
+public class Shot {
+    int x;
+    int y;
+
+    public Shot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
